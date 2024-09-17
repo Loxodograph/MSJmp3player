729aea31618c7790c91ba90ec0838562aff823ca
loxodograph <paldino.nicholas@gmail.com>
2024-09-17 17:00:58 -0400

    testing mobile

--- script.js
+++ script.js
@@ -124,9 +124,9 @@ const playSong = (id) => {
 
 const changeImage =(id) =>{
   if (id >= 8){
-    image.setAttribute("src", "./MP3 Player/lavender.png")
+    image.setAttribute("src", "./MP3 Player/Lavender.png")
 } else {
-  image.setAttribute("src", "./MP3 Player/damlife.jpg")
+    image.setAttribute("src", "./MP3 Player/DamLife.jpg")
 }
 }
 
